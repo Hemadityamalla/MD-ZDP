c8d9e444d8293b25dfbbe7d46a447c30726af900
Hemadityamalla <hemadityamalla@gmail.com>
2021-12-08 16:58:30 +0100

    First steps at variable time stepping to avoid simulation explosion

--- src/main.f90
+++ src/main.f90
@@ -9,14 +9,14 @@ program zeroDimPlasmaChem
     type(ode_sys) :: odes
     integer, parameter :: n_cell = 1
     integer, parameter :: dt_num_cond = 2
-    real(dp) :: dt_array(2)
+    real(dp) :: dt_array(1:2)
     integer, parameter :: dt_ix_chem = 1
     integer, parameter :: dt_ix_drt = 2
     real(dp), parameter :: dt_safety_factor = 0.9_dp
     real(dp) :: time, global_dt
     real(dp) :: end_time
     real(dp) :: dt_max = 1e-11_dp
-    real(dp) :: dt_min = 1e-14_dp
+    real(dp) :: dt_min = 1e-12_dp
     character(len=name_len) :: integrator = "heuns_method"
     integer :: time_integrator = -1
     integer, parameter :: n_integrators = 3
@@ -36,7 +36,7 @@ program zeroDimPlasmaChem
     integer :: test_iterator
     !character(len=string_len) :: output_name
 
-    print *, "Inside main prog:", odes%n_vars
+    print *, "Inside main prog:"
     call CFG_update_from_arguments(cfg)
     call init_modules(cfg, odes)
     
@@ -63,7 +63,7 @@ program zeroDimPlasmaChem
     do while (time < end_time)
       ! Add functionality to compute the wall clock time 
 
-      !print *, "Time: ", time
+      print *, "Time: ", time
       call output_HDF5_solution(odes, trim(output_name), time, test_iterator)
       call ode_advance(odes, global_dt, &
          species_itree(n_gas_species+1:n_species), time_integrator)
@@ -72,6 +72,9 @@ program zeroDimPlasmaChem
       !test_varnames = pack(odes%var_names, odes%var_matrix==1)
       !print *, "Varnames: ",pack(odes%vars, odes%var_matrix==1) 
       !print *, "Num actual vars: ", sum(odes%var_matrix)
+      call dt_constraints(dt_array, odes)
+      !print *, "dt array: ", dt_array
+      global_dt = min(minval(dt_array), dt_max)
       time = time + global_dt
       test_iterator = test_iterator + 1
 
@@ -82,7 +85,7 @@ program zeroDimPlasmaChem
     call output_HDF5_solution(odes, trim(output_name), time, test_iterator)
     
 
-    print *, "End of simulation"
+    print *, "End of simulation at t= ", time
 
     contains
 
@@ -146,19 +149,19 @@ program zeroDimPlasmaChem
     end subroutine init_variables
 
 
-    subroutine print_var_vals(o_s)
-      implicit none
-      type(ode_sys),intent(in) :: o_s
-      integer, allocatable :: var_matrix(:)
-      integer :: n_vars
-      integer :: i
-      n_vars = o_s%n_vars
-      !allocate(var_matrix(n_vars))
-      var_matrix = o_s%var_matrix
+    !subroutine print_var_vals(o_s)
+    !  implicit none
+    !  type(ode_sys),intent(in) :: o_s
+    !  integer, allocatable :: var_matrix(:)
+    !  integer :: n_vars
+    !  integer :: i
+    !  n_vars = o_s%n_vars
+    !  !allocate(var_matrix(n_vars))
+    !  var_matrix = o_s%var_matrix
 
-      
+    !  
     
-    end subroutine print_var_vals
+    !end subroutine print_var_vals
 
     subroutine dt_initialize(cfg)
         use m_config
@@ -348,6 +351,35 @@ program zeroDimPlasmaChem
     
     end subroutine ode_advance
 
+    subroutine dt_constraints(dt, o_s)
+      use m_chemistry
+      use m_transport_data
+      use m_units_constants
+      use m_table_data
+      use m_lookup_table
+      use m_gas
+      implicit none
+      type(ode_sys),intent(inout) :: o_s
+      real(dp),intent(inout) :: dt(2)
+      real(dp) ::  ne, E_vm
+      real(dp) :: mobility
+      real(dp), parameter :: eps = 1e-100_dp
+      real(dp) :: Td
+
+    !Computing the dielectric relaxation time
+    ne = o_s%vars(i_electron)
+    E_vm = o_s%vars(4) !TODO:make this better
+    Td = E_vm*SI_to_Townsend/gas_number_density
+    mobility =  LT_get_col(td_tbl, td_mobility, Td)/gas_number_density
+    dt(dt_ix_drt) = UC_eps0/max(UC_elem_charge*mobility*ne, eps)
+    !Computing the chemistry time
+    dt(dt_ix_chem)  = maxval(o_s%vars(& 
+      species_itree(n_gas_species+1:n_species))) & 
+      / max(maxval( &
+      o_s%vars_rhs(species_itree(n_gas_species+1:n_species))), eps)
+    
+    end subroutine dt_constraints
+
     !Subroutine that outputs the simulation data as a .txt file
     subroutine output_solution(odes_s, filename, t, idx)
       use m_chemistry
@@ -387,65 +419,5 @@ program zeroDimPlasmaChem
       close(my_unit)
     end subroutine output_solution
     
-    !subroutine output_HDF5_solution(odes_s, filename, t, idx)
-    !  use m_chemistry
-    !  use m_types
-    !  use HDF5
-    !  implicit none
-    !  type(ode_sys), intent(in) :: odes_s
-    !  character(len=*), intent(in) :: filename
-    !  character(len=50), save :: fmt, fmt_header
-    !  character(len=50) :: test_fmt
-    !  integer :: my_unit, n, i, n_vars, error, space_rank
-    !  integer, intent(in) :: idx
-    !  real(dp), intent(in) :: t
-    !  logical, save :: first_time = .true.
-    !  
-    !  integer(HSIZE_T) :: data_dims(1)
-    !  integer(HID_T) :: file_id, dspace_id
-    !  integer(HID_T), allocatable :: dset_id(:)
-    !  integer :: max_rows = 5
-
-    !  
-
-    !  n_vars = odes_s%n_vars
-    !  allocate(dset_id(n_vars))
-    !  if (first_time) then
-    !     first_time = .false.
-
-    !     call h5open_f(error)
-
-    !     call h5fcreate_f(trim(filename), H5F_ACC_TRUNC_F, file_id, error)
-
-    !     space_rank = 1
-    !     data_dims(1) = max_rows
-
-    !     call h5screate_simple_f(space_rank, data_dims, dspace_id, error)
-    !     do i=1,n_vars
-    !            call h5dcreate_f(file_id, trim(odes_s%var_names(i)), H5T_NATIVE_DOUBLE, dspace_id, dset_id(i), error)
-    !            call h5dclose_f(dset_id(i), error)
-
-    !     end do
-
-    !     
-
-
-
-
-    !     call h5fclose_f(file_id, error)
-
-    !     call h5close_f(error)
-    !     
-
-    !  end if
-
-    !  !write(fmt, "A, I0, A"), 
-    !  !fmt = "(E16.8, E16.8)"
-    !  !open(newunit=my_unit, file=trim(filename), action="write", &
-    !  !   position="append")
-
-    !  !write(my_unit, fmt) t, odes_s%vars(idx)
-    !  !close(my_unit)
-    !end subroutine output_HDF5_solution
 end program zeroDimPlasmaChem
 
